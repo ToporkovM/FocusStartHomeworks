4dca56479ce5fa05c74f6ba03fd3bda172eb5b34
max <toporkovmaksim7@gmail.com>
2020-10-16 15:48:57 +0700

    создание проекта

---
+++ Homeworks-1-Toporkov/Homeworks-1-Toporkov/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homeworks-1-Toporkov
+//
+//  Created by Максим Топорков on 16.10.2020.
+//  Copyright © 2020 Максим Топорков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
